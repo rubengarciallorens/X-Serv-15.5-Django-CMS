eb1a06e4b1bd3c8127df6b820ae888418ee97b02
rubengarciallorens <rubengl95@gmail.com>
2018-04-24 17:39:11 +0200

    .2

--- cms/views.py
+++ cms/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
-from django.http import Httpresponse
+from django.http import HttpResponse
+from models import Pages
 
 # Create your views here.
+def list (request, id):
+	try:
+		page=Pages.Objects.Get(id=identification)
+		response=page.page
+	except:
+		response="Page " + str(identification) + "is not saved \n"
+
+	return HttpResponse(response)
