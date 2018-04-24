fba8066af090b1a4ceaff64b6e6a36d0fd5d3248
rubengarciallorens <rubengl95@gmail.com>
2018-04-24 18:55:01 +0200

    ...

--- cms/views.py
+++ cms/views.py
@@ -3,11 +3,11 @@ from django.http import HttpResponse
 from models import Pages
 
 # Create your views here.
-def list (request, id):
+def list (request, identification):
 	try:
-		page=Pages.Objects.Get(id=identification)
+		page=Pages.objects.get(id=identification)
 		response=page.page
 	except:
-		response="Page " + str(identification) + "is not saved \n"
+		response="Page:  " + str(identification) + " is not saved \n"
 
 	return HttpResponse(response)
